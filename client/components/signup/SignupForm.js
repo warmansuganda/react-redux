13e7413331289b9a83786727130ba1d5e02edd3f
envpoint <envpoint@gmail.com>
2017-06-14 22:16:42 +0700

    make form signup

---
+++ client/components/signup/SignupForm.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import map from 'lodash/map';
+import timezones from '../../data/timezones';
+
+class SignupForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username : '',
+            email : '',
+            password : '',
+            passwordConfirmation : '',
+            timezone : ''
+        }
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
+        const optTimezone = map(timezones, (val, key) => 
+            <option key={val} value={val}>{key}</option> 
+        );
+        return (
+            <form onSubmit={this.onSubmit}>
+                <h1>Join with our community!</h1>
+
+                <div className="form-group">
+                    <label className="control-label">Username</label>
+                    <input 
+                        type="text" 
+                        name="username" 
+                        value={this.state.username}
+                        onChange={this.onChange}
+                        className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Email</label>
+                    <input 
+                        type="text" 
+                        name="email" 
+                        value={this.state.email}
+                        onChange={this.onChange}
+                        className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Password</label>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        value={this.state.password}
+                        onChange={this.onChange}
+                        className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Password Confirmation</label>
+                    <input 
+                        type="password" 
+                        name="passwordConfirmation" 
+                        value={this.state.passwordConfirmation}
+                        onChange={this.onChange}
+                        className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Timezone</label>
+                    <select
+                        name="timezone" 
+                        value={this.state.timezone}
+                        onChange={this.onChange}
+                        className="form-control">
+                        <option value="" disabled>Select your timezone</option>
+                        {optTimezone}
+                    </select>
+                </div>
+
+                <div className="form-group">
+                    <button className="btn btn-primary btn-lg">Sign Up</button>
+                </div>
+
+            </form>
+        );
+    }
+}
+
+export default SignupForm;
--- client/components/signup/SignupPage.js
+++ client/components/signup/SignupPage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import NavigationBar from '../NavigationBar';
 
+import SignupForm from './SignupForm';
+
 class SignupPage extends React.Component {
     render() {
         return (
             <div>
                 <NavigationBar />
                 <div className="jumbotron">
-                    <h1>Signup Page</h1>
+                    <SignupForm />
                 </div>
             </div>
         )
