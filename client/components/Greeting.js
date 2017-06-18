0d7f5453e688ec49fe09b59f7e57faba4e49dab8
envpoint <envpoint@gmail.com>
2017-06-18 11:41:25 +0700

    add delete flash message

--- client/components/Greeting.js
+++ client/components/Greeting.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavigationBar from './NavigationBar';
+import FlashMessagesList from './flash/FlashMessagesList';
 
 export default () => {
     return (
         <div>
             <NavigationBar />
+            <FlashMessagesList />
             <div className="jumbotron">
                 <h1>Hello react from class greeting!</h1>
             </div>
