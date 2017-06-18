e69024fe2c47ef225d291ba9398de0104108c491
envpoint <envpoint@gmail.com>
2017-06-18 15:46:25 +0700

    add models for save users to database

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 
 import validateInput from '../shared/validations/signup';
+import Users from '../models/Users';
 
 let router = express.Router();
 
@@ -9,7 +11,17 @@ router.post('/', (req, res) => {
     const { errors, isValid } = validateInput(req.body);
 
     if (isValid) {
-        res.json({success: true});
+        // res.json({success: true});
+        const {username, password, email, timezone} = req.body;
+        const password_digest = bcrypt.hashSync(password, 10);
+
+        Users.forge({
+            username, password_digest, email, timezone
+        }, {hasTimestamps: true}).save().then(user => 
+            res.json({success: true})
+        ).catch(err =>
+            res.status(500).json({error: err})
+        );
     } else {    
         res.status(400).json(errors);
     }
